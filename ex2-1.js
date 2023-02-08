e431db6b7b60ef404fe9574f835a64d31ef1bf75
heewon.kim <hewon1025@naver.com>
2023-02-08 23:52:35 +0900

    첫 프로젝트 시작
    js를 실행하는 것
    http 모듈로 서버 만들기

---
+++ ex2-1.js
@@ -0,0 +1,20 @@
+function frist(){
+    second();
+    console.log('첫 번째');
+}
+function second(){
+    third();
+    console.log('두 번째');
+}
+function third(){
+    console.log('세 번째');
+}
+
+function run(){
+    console.log('3초 후 실행');
+}
+console.log('시작');
+setTimeout(run,3000);
+console.log('끝');
+
+
